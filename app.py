b61007372eb449493a8413146944e089bff7aea4
Anirban Sarkar <sanirban2006@gmail.com>
2025-09-14 20:47:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model = joblib.load("models/decision_tree_model.pkl")
 
 # Title
 st.set_page_config(page_title="Are You Single or Committed?", page_icon="💘")
-st.title("💘 Fun Relationship Status Predictor")
+st.title("💘Relationship Status Predictor")
 st.write("Answer the following lifestyle questions and find out whether you are **Single** or **Committed** (just for fun 😉).")
 
 # Questions (features)
@@ -17,14 +17,14 @@ male_friends = st.slider("👨 Number of close male friends", 0, 20, 5)
 female_friends = st.slider("👩 Number of close female friends", 0, 20, 5)
 party_freq = st.slider("🎉 Parties attended per month", 0, 15, 2)
 late_night_talks = st.slider("📞 Average late-night calls/chats in month", 0, 14, 2)
-study_hours = st.slider("Study Hours per Day in week(self study)", 0, 25, 4)
+study_hours = st.slider("Study Hours per Week(self study)", 0, 25, 4)
 Messaging_Apps_Used = st.slider("Messaging app used daily..(count)", 0, 5, 2)
-Texts_Per_Day = st.slider(" Number of texts per done day ", 0, 200, 50)
+Texts_Per_Day = st.slider("Number of texts per done day ", 0, 200, 50)
 Parents_Strictness_Level = st.slider("Rate your parents on their strictness...", 0, 5, 1)
 Coffee_Shop_Visits = st.slider("Coffe shop or cafe visits per month", 0, 10, 1)
 gender = st.radio("⚧️ Your gender", ["Female", "Male"])
 
-# Convert gender
+# Convert gender (male or female)
 gender_value = 0 if gender == "Female" else 1
 
 # Collect input into dataframe
@@ -52,3 +52,4 @@ if st.button("🔮 Predict My Relationship Status"):
     st.pyplot(fig)
 
     st.success("✨ This is just for fun! Don’t take it too seriously 😉")
+
